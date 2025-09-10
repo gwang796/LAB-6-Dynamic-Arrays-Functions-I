e061af46f232d2942ee17e351120eedd7713aab4
Guo An Wang <you@example.com>
2025-09-10 16:59:12 -0700

    Initial commit

---
+++ Lab 6: Dynamic Arrays & Functions I/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMSC-210 | Lab 6: Dynamic Arrays & Functions I | Guo An Wang
+//  IDE used: Xcode
+//  Created by Guo An Wang on 9/10/25.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
