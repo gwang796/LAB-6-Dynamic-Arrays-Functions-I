fcb2eb49eb335a3dd5cb3545b3b5d2068dc07c92
Guo An Wang <you@example.com>
2025-09-10 17:11:07 -0700

    second commit

--- Lab 6: Dynamic Arrays & Functions I/main.cpp
+++ Lab 6: Dynamic Arrays & Functions I/main.cpp
@@ -8,8 +8,37 @@
 #include <iostream>
 using namespace std;
 
+//function enterArrayData has user enter in data
+//argument: double array
+//return: none
+void enterArrayData(double *arr, int SIZE);
+
+//function outputArrayData prints inputted data
+//argument: double array
+//return: none
+void outputArrayData(double *arr, int SIZE);
+
+//function sumArray adds all inputted data together
+//argument: double array
+//return: none
+void sumArray(double *arr, int SIZE);
+
+const int WIDTH = 5;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    const int SIZE = 5;
+    double *dArray = nullptr;
+    dArray = new double[SIZE];
+    //enterArrayData(dArray,SIZE)
+    //outputArrayData(dArray,SIZE)
+    //sumArrray(dArray,SIZE)
     return 0;
+}
+
+void enterArrayData(double *arr, int SIZE){
+    cout << "Data entry for the array:" << endl;
+    for (int i = 0; i < SIZE; i++) {
+        cout << set(WIDTH) << "> Element #" << i << ": ";
+        
+    }
 }
